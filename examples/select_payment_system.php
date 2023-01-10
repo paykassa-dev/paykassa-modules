687207edb46542a802265b59de8373d3daef2b95
Paykassa Dev <dev@paykassa.pro>
2023-01-10 05:15:08 +0300

    Add new features

---
+++ examples/select_payment_system.php
@@ -0,0 +1,43 @@
+<?php
+
+    require_once __DIR__ . "/../src/paykassasci.class.php";
+    require_once __DIR__ . "/../src/paykassaapi.class.php";
+
+
+    $secret_keys_and_config = [
+        "merchant_id" => "Merchant ID",
+        "merchant_password" => "Merchant Password",
+        "api_id" => "API ID",
+        "api_password" => "API Password",
+        "config" => [
+            "test_mode" => false,
+        ],
+    ];
+
+
+
+
+    //$list = PaykassaSCI::get_payment_systems();
+    $list = PaykassaAPI::get_payment_systems();
+
+    ?>
+
+
+    <form action="" method="POST">
+        <label>Select payment direction</label>
+        <select name="pscur">
+            <option value="">---</option>
+            <?php foreach ($list as $item) { ?>
+                <?php foreach ($item["currency_list"] as $currency) { ?>
+                    <option value="<?php echo htmlspecialchars(
+                        sprintf("%s_%s", strtolower($item["system"]), strtolower($currency)),
+                        ENT_QUOTES, "UTF-8"); ?>">
+                        <?php echo htmlspecialchars(sprintf("%s %s", $item["display_name"], $currency),
+                            ENT_QUOTES, "UTF-8"); ?>
+                    </option>
+                <?php } ?>
+            <?php } ?>
+        </select>
+
+        <button>To pay</button>
+    </form>
